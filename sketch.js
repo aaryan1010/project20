e67e00bccc09de6a10848caf502c08ace453296d
aaryan1010 <70142966+aaryan1010@users.noreply.github.com>
2020-11-24 16:44:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,5 +12,18 @@ function setup() {
 
 function draw() {
   background("lightblue");  
+  if(wall.x-car.x < (car.width+wall.width)/2) {
+     car.velocityX=0; 
+     var deformation=0.5 * weight * speed* speed/22500; 
+     if(deformation>180) { 
+       car.shapeColor=color("yellow"); 
+    }
+     if(deformation<180 && deformation>100) { 
+      car.shapeColor=color("red"); 
+    }
+     if(deformation<100) { 
+       car.shapeColor=color("green");
+       }
+       }
   drawSprites();
 }
